a53e3c56fb84ce1a1c4643ce808639b0074bdee7
phptuts <glaserpower@gmail.com>
2024-08-04 21:41:38 -0700

    Got it sending to the electron app

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("communication", {
-  message: (settings) => ipcRenderer.invoke("settings", settings),
-  // we can also expose variables, not just functions
+  setSettings: (settings) => ipcRenderer.invoke("settings", settings),
 });
